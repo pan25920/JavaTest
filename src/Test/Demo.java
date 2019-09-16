0fbc31abf093befc828d44fe5355f6a1f4b31c38
pan25920 <14030416@qq.com>
2019-09-16 19:57:18 +0800

    Update Demo.java

--- src/Test/Demo.java
+++ src/Test/Demo.java
@@ -3,14 +3,6 @@ package Test;
 public class Demo{
     public static void main(String[] args) {
 
-        int a=1;
-
-        int b=1;
-
-        Student student=new Student();
-        int aa=student.inta;
-        System.out.println(a==aa);
-
-
+        int a=0;
     }
 }
